51a85c0f21b3d7eb8471377189b4c704cc93793a
Barbarakovacs85 <133600947+Barbarakovacs85@users.noreply.github.com>
2024-06-10 15:16:16 +0200

    add calculation

--- mvc/models/Calculation.php
+++ mvc/models/Calculation.php
@@ -1,4 +1,5 @@
-<?php 
+
+<?php
 
 namespace calculation;
 
@@ -10,9 +11,35 @@ class Calculation {
 
     public function __construct($num1, $num2, $operator) {
         $this->num1 = $num1;
-        $this->$num2 = $num2;
+        $this->num2 = $num2;
         $this->operator = $operator;
     }
-}
 
-?>
+    public function calculate() {
+        switch ($this->operator) {
+            case 'Összeadás':
+                $this->total = $this->num1 + $this->num2;
+                break;
+            case 'Kivonás':
+                $this->total = $this->num1 - $this->num2;
+                break;
+            case 'Szorzás':
+                $this->total = $this->num1 * $this->num2;
+                break;
+            case 'Osztás':
+                if ($this->num2 != 0) {
+                    $this->total = $this->num1 / $this->num2;
+                } else {
+                    throw new \Exception('Nullával való osztás nem lehetséges');
+                }
+                break;
+            default:
+                throw new \Exception('Érvénytelen művelet');
+        }
+        return $this->total;
+    }
+    
+    public function getTotal() {
+        return $this->total;
+    }
+}
