2090893e65e92a5bec10bef6fd7facaac7f0de02
Barbarakovacs85 <133600947+Barbarakovacs85@users.noreply.github.com>
2024-06-11 20:12:33 +0200

    add calculator correction

--- mvc/models/NewCalculator.php
+++ mvc/models/NewCalculator.php
@@ -1,9 +1,30 @@
 <?php
-
 namespace models;
 
-class Newcalculator extends Calculation {
+class NewCalculator extends Calculation {
+   
     public function __construct($num1, $num2, $operator) {
         parent::__construct($num1, $num2, $operator);
     }
+
+    public function calculate() {
+        switch ($this->operator) {
+            case 'Négyzetre emelés':
+                $this->total = $this->num1 * $this->num1;
+                break;
+            case 'Négyzetgyökvonás':
+                if ($this->num1 >= 0) {
+                    $this->total = sqrt($this->num1);
+                } else {
+                    $_SESSION["error"] = 'Negatív számnak nincs valós négyzetgyöke';
+                    return;
+                }
+                break;
+            default:
+                
+                return parent::calculate();
+        }
+        return $this->total;
+    }
 }
+?>
